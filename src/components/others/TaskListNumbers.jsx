4443d976f8097092fd62fa22a7622c829f116962
Varun Behere <varunbehere@gmail.com>
2024-11-09 19:30:52 +0000

    Make component reusable

--- src/components/others/TaskListNumbers.jsx
+++ src/components/others/TaskListNumbers.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+export const TaskStatusCard = ({ count, type, color }) => (
+  <>
+    <div className={`rounded-xl w-full sm:w-[45%] py-6 px-10 bg-gradient-to-r from-${color}-500 to-${color}-600 shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105`}>
+      <h2 className="text-4xl font-bold text-white mb-1">{count}</h2>
+      <h3 className="text-lg font-medium text-gray-200">{type}</h3>
+    </div>
+  </>
+)
 
 const TaskListNumbers = ({ data }) => {
+
+  const [counts, setCounts] = useState({})
+  const taskStatuses = [
+    { type: 'New Task', color: 'blue', cardKey: 'newTask' },
+    { type: 'In Progress', color: 'indigo', cardKey: 'active' },
+    { type: 'Completed', color: 'green', cardKey: 'completed' },
+    { type: 'Failed Task', color: 'red', cardKey: 'failed' },
+  ]
+
+  useEffect(() => {
+    setCounts(data.taskCount)
+  }, [data])
+
   return (
-
-
     <div className="flex mt-10 justify-between gap-5 flex-wrap">
-      {/* New Task Card */}
-      <div className="rounded-xl w-full sm:w-[45%] py-6 px-10 bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105">
-        <h2 className="text-4xl font-bold text-white mb-1">{data.taskCount.newTask}</h2>
-        <h3 className="text-lg font-medium text-gray-200">New Task</h3>
-      </div>
-
-      {/* In Progress Card */}
-      <div className="rounded-xl w-full sm:w-[45%] py-6 px-10 bg-gradient-to-r from-indigo-500 to-indigo-600 shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105">
-        <h2 className="text-4xl font-bold text-white mb-1">{data.taskCount.active}</h2>
-        <h3 className="text-lg font-medium text-gray-200">In Progress</h3>
-      </div>
-
-      {/* Completed Task Card */}
-      <div className="rounded-xl w-full sm:w-[45%] py-6 px-10 bg-gradient-to-r from-green-500 to-green-600 shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105">
-        <h2 className="text-4xl font-bold text-white mb-1">{data.taskCount.completed}</h2>
-        <h3 className="text-lg font-medium text-gray-200">Completed</h3>
-      </div>
-
-      {/* Failed Task Card */}
-      <div className="rounded-xl w-full sm:w-[45%] py-6 px-10 bg-gradient-to-r from-red-500 to-red-600 shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105">
-        <h2 className="text-4xl font-bold text-white mb-1">{data.taskCount.failed}</h2>
-        <h3 className="text-lg font-medium text-gray-200">Failed Task</h3>
-      </div>
+      {taskStatuses.map((elem, idx) => (
+        <TaskStatusCard key={elem.cardKey+idx} count={counts[elem.cardKey]} type={elem.type} color={elem.color} />
+      ))}
     </div>
   )
 }
-
 export default TaskListNumbers;
