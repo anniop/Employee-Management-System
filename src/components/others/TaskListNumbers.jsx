81ba27420bff4bb0e55e6cc40d0b85613779ffe1
Varun Behere <varunbehere@gmail.com>
2024-11-23 05:00:26 +0000

    Update the list number card component

--- src/components/others/TaskListNumbers.jsx
+++ src/components/others/TaskListNumbers.jsx
@@ -1,14 +1,44 @@
+import React, { useEffect, useState } from "react";
 
-import React, { useState, useEffect } from 'react';
-import { TaskStatusCard } from './TaskStatusCard';
+export const TaskStatusCard = ({ count, type, textColor, borderColor }) => {
+  return (
+    <div
+      className={`rounded-t-xl w-full sm:w-[45%] lg:w-[22%] py-8 px-12 bg-gradient-to-br from-gray-800 to-gray-900 shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col justify-center items-center border-b-4 ${borderColor}`}
+      aria-label={`${count} ${type}`}
+    >
+      <h2 className={`text-4xl font-bold ${textColor} mb-2`}>{count}</h2>
+      <h3 className={`text-lg font-medium text-gray-400`}>{type}</h3>
+    </div>
+  );
+};
 
 const TaskListNumbers = ({ data }) => {
   const [counts, setCounts] = useState({});
   const taskStatuses = [
-    { type: 'New Task', color: 'blue', cardKey: 'newTask' },
-    { type: 'In Progress', color: 'indigo', cardKey: 'active' },
-    { type: 'Completed', color: 'green', cardKey: 'completed' },
-    { type: 'Failed Task', color: 'red', cardKey: 'failed' },
+    {
+      type: "New Task",
+      textColor: "text-blue-400",
+      borderColor: "border-blue-400",
+      cardKey: "newTask",
+    },
+    {
+      type: "In Progress",
+      textColor: "text-indigo-400",
+      borderColor: "border-indigo-400",
+      cardKey: "active",
+    },
+    {
+      type: "Completed",
+      textColor: "text-green-400",
+      borderColor: "border-green-400",
+      cardKey: "completed",
+    },
+    {
+      type: "Failed Task",
+      textColor: "text-red-400",
+      borderColor: "border-red-400",
+      cardKey: "failed",
+    },
   ];
 
   useEffect(() => {
@@ -16,13 +46,14 @@ const TaskListNumbers = ({ data }) => {
   }, [data]);
 
   return (
-    <div className="flex mt-10 justify-between gap-8 flex-wrap">
-      {taskStatuses.map((elem, idx) => (
+    <div className="flex flex-wrap justify-between mt-10 gap-6">
+      {taskStatuses.map((elem) => (
         <TaskStatusCard
-          key={elem.cardKey + idx}
-          count={counts[elem.cardKey] || 0} // Default to 0 if count is undefined
+          key={elem.cardKey}
+          count={counts[elem.cardKey] || 0}
           type={elem.type}
-          color={elem.color}
+          textColor={elem.textColor}
+          borderColor={elem.borderColor}
         />
       ))}
     </div>
